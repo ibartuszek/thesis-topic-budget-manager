0c85d1aa098235af42fd6614a18843f3f81d7141
Istvan Bartuszek <istvan.bartuszek@gmail.com>
2019-07-14 10:35:18 +0200

    First commit

---
+++ budgetmanager-transaction-service/src/main/java/hu/elte/bm/budgetmanagertransactionservice/BudgetmanagerTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package hu.elte.bm.budgetmanagertransactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetmanagerTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetmanagerTransactionServiceApplication.class, args);
+	}
+
+}
