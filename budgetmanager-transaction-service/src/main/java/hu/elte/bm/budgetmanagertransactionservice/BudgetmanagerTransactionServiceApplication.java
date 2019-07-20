42c73015049800cd8f5cddfe5addc4c75d546cb4
Istvan Bartuszek <istvan.bartuszek@gmail.com>
2019-07-20 12:30:09 +0200

    Init transaction-service

--- budgetmanager-transaction-service/src/main/java/hu/elte/bm/budgetmanagertransactionservice/BudgetmanagerTransactionServiceApplication.java
+++ budgetmanager-transaction-service/src/main/java/hu/elte/bm/budgetmanagertransactionservice/BudgetmanagerTransactionServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BudgetmanagerTransactionServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BudgetmanagerTransactionServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetmanagerTransactionServiceApplication.class, args);
+    }
 
 }
